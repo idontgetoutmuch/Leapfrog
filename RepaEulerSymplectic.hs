209d6c7538fbf02f5384a1ebd0d6049ebcd38f78
Dominic Steinitz <dominic@steinitz.org>
2013-07-04 18:07:54 +0100

    A working simple version of the Euler symplectic method using repa.

--- RepaEulerSymplectic.hs
+++ RepaEulerSymplectic.hs
@@ -12,13 +12,9 @@ import qualified Initial as I
 
 import Data.Array.Repa hiding ((++), zipWith)
 import qualified Data.Array.Repa as Repa
-import Data.Array.Repa.Algorithms.Matrix
 import Control.Monad
-import Control.Monad.Identity
-import Text.Printf
-import qualified Data.List as L
 
-stepPositionP :: forall a b c m . ( Monad m
+stepPosition :: forall a b c m . ( Monad m
                  , Source a Double
                  , Source b Double
                  , Source c Double
@@ -28,7 +24,7 @@ stepPositionP :: forall a b c m . ( Monad m
                  Array b DIM1 Double ->
                  Array c DIM2 Double ->
                  m (Array U DIM2 Double)
-stepPositionP h qs ms ps = do
+stepPosition h qs ms ps = do
   do newQs <- computeP $ qs +^ (ps *^ h2 /^ ms2)
      return newQs
     where
@@ -37,7 +33,7 @@ stepPositionP h qs ms ps = do
       h2  = extend (Any :. i :. j) $ fromListUnboxed Z [h]
       ms2 = extend (Any :. j) ms
 
-stepMomentumP :: forall a b c m . ( Monad m
+stepMomentum :: forall a b c m . ( Monad m
                  , Source a Double
                  , Source b Double
                  , Source c Double
@@ -48,7 +44,7 @@ stepMomentumP :: forall a b c m . ( Monad m
                  Array b DIM1 Double ->
                  Array c DIM2 Double ->
                  m (Array U DIM2 Double)
-stepMomentumP gConst h qs ms ps =
+stepMomentum gConst h qs ms ps =
   do fs <- sumP $ transpose $ zeroDiags' fss
      newPs <- computeP $ ps +^ (fs *^ dt2)
      return newPs
@@ -64,10 +60,11 @@ stepMomentumP gConst h qs ms ps =
     preFs = Repa.map (* (negate gConst)) $
             qDiffs /^ ds
     fss = is *^ preFs
-    Z :.i :. j :. k = extent fss
-    dt2             = extend (Any :. i :. k) $ fromListUnboxed Z [h]
+    
+    Z :.i :. _j :. k = extent fss
+    dt2              = extend (Any :. i :. k) $ fromListUnboxed Z [h]
 
-stepOnceP :: ( Monad m
+stepOnce :: ( Monad m
              , Source a Double
              , Source b Double
              , Source c Double
@@ -78,31 +75,49 @@ stepOnceP :: ( Monad m
              Array a DIM2 Double ->
              Array c DIM2 Double ->
              m (Array U DIM2 Double, Array U DIM2 Double)
-stepOnceP gConst h ms qs ps = do
-  newPs <- stepMomentumP gConst h qs ms ps
-  newQs <- stepPositionP h qs ms newPs
+stepOnce gConst h ms qs ps = do
+  newPs <- stepMomentum gConst h qs ms ps
+  newQs <- stepPosition h qs ms newPs
   return (newQs, newPs)
 
--- FIXME
+repDim2to3Outer :: Source a Double =>
+                   Array a DIM2 Double ->
+                   Array D DIM3 Double
 repDim2to3Outer a = extend (Any :. I.spaceDim) a
-
 
 type Momenta   = Array U DIM2 Double
 type Positions = Array U DIM2 Double
 type Masses    = Array U DIM1 Double
 
+zeroDiags :: Source a Double =>
+             Array a DIM2 Double ->
+             Array D DIM2 Double
 zeroDiags x = traverse x id f
   where
     f _ (Z :. i :. j) | i == j    = 0.0
                       | otherwise = x!(Z :. i :. j)
                                     
+zeroDiags' :: Source a Double =>
+              Array a DIM3 Double ->
+              Array D DIM3 Double
 zeroDiags' x = traverse x id f
   where
     f _ (Z :. i :. j :. k) | i == j    = 0.0
                            | otherwise = x!(Z :. i :. j :. k)
 
-hamiltonianP :: Double -> Masses -> Momenta -> Positions -> IO Double
-hamiltonianP gConst ms qs ps = do
+potentialEnergy :: Double -> Masses -> Positions -> IO (Array U DIM1 Double)
+potentialEnergy gConst ms qs = do
+  putStrLn $ show qs
+  ds2 <- sumP $ Repa.map (^2) $ pointDiffs qs
+  let ds   = Repa.map sqrt ds2
+      is   = prodPairsMasses ms
+      pess = zeroDiags $ Repa.map (* (negate gConst)) $ is /^ ds
+  dsU :: Array U DIM2 Double <- computeP ds
+  putStrLn $ show dsU
+  sumP pess
+
+hamiltonian :: Double -> Masses -> Momenta -> Positions -> IO Double
+hamiltonian gConst ms qs ps = do
   preKes <- sumP $ ps *^ ps
   ke     <- sumP $ preKes /^ ms
 
@@ -150,36 +165,15 @@ replicateRows a = extend (Any :. i :. All) a
 
 type Distance = Double
 type Mass     = Double
-type Force    = Double
-type Speed    = Double
-type Energy   = Double
 
 initPs :: Array U DIM2 Double
-initPs = runIdentity $ computeP $ ms2 *^ initVs
-  where
-    (Z :. i :. j) = extent initVs
-    ms2 = extend (Any :. j) masses
+initPs = fromListUnboxed (Z :. I.nBodiesTwoPlanets :. I.spaceDim) $ concat I.initPs
 
-initRs :: Array U DIM2 Distance
-initRs = fromListUnboxed (Z :. nBodies :. spaceDim) $ concat xs
-  where
-    nBodies = length xs
-    xs = [ [earthX,   earthY,   earthZ]
-         , [jupiterX, jupiterY, jupiterZ]
-         , [sunX,     sunY,     sunZ]
-         ]
-    (earthX,   earthY,   earthZ)   = earthR
-    (jupiterX, jupiterY, jupiterZ) = jupiterR
-    (sunX,     sunY,     sunZ)     = sunR
+initQs :: Array U DIM2 Distance
+initQs = fromListUnboxed (Z :. I.nBodiesTwoPlanets :. I.spaceDim) $ concat $ I.initQs
 
 masses :: Array U DIM1 Mass
-masses = fromListUnboxed (Z :. nBodies) xs
-  where
-    nBodies = length xs
-    xs = [ earthMass
-         , jupiterMass
-         , sunMass
-         ]
+masses = fromListUnboxed (Z :. I.nBodiesTwoPlanets) I.massesTwoPlanets
 
 stepN :: forall m . Monad m =>
          Int -> Double -> Double -> Masses -> Positions -> Momenta ->
@@ -188,7 +182,15 @@ stepN n gConst dt masses = curry updaterMulti
   where
     updaterMulti = foldr (>=>) return updaters
     updaters :: [(Positions, Momenta) -> m (Positions, Momenta)]
-    updaters = replicate n (uncurry (stepOnceP gConst dt masses))
+    updaters = replicate n (uncurry (stepOnce gConst dt masses))
 
-
-
+main :: IO ()
+main = do
+  potEnPre <- potentialEnergy I.gConst masses initQs
+  putStrLn $ show potEnPre
+  hPre <- hamiltonian I.gConst masses initQs initPs
+  putStrLn $ show hPre
+  (newQs, newPs) <- stepN 100 I.gConst I.stepTwoPlanets masses initQs initPs
+  hPost <- hamiltonian I.gConst masses newQs newPs
+  putStrLn $ show hPost
+  putStrLn "Hello world"
