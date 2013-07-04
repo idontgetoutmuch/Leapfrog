d9f456af471e473e09a72dcac9864139e20a3fe8
Dominic Steinitz <dominic@steinitz.org>
2013-07-04 16:19:33 +0100

    Calculate the Hamiltonian. Sadly this seems to vary for the same initial conditions.

--- YarrEulerSymplectic.hs
+++ YarrEulerSymplectic.hs
@@ -82,25 +82,62 @@ stepOnce gConst h ms qs ps = do
   stepMomentum gConst h qs ms ps
   stepPosition h qs ms ps
 
+-- potentialEnergy :: Double -> Masses -> Positions -> IO (Array Double)
+-- potentialEnergy gConst ms qs = do
+--   pes :: Array Double <- new I.nBodiesTwoPlanets
+--   let peOnePairParticles :: Int ->
+--                             Int ->
+--                             IO Double
+--       peOnePairParticles i j
+--         | i == j = return 0.0
+--         | otherwise = do
+--           q1 <- qs `index` i
+--           m1 <- ms `index` i
+--           q2 <- qs `index` j
+--           m2 <- ms `index` j
+--           let qDiffs = V.zipWith (-) q1 q2
+--               dist2  = V.sum $ V.map (^2) qDiffs
+--               a      = 1.0 / dist2
+--               b      = (negate gConst) * m1 * m2 * (sqrt a)
+--           return $ V.sum $ V.map (*b) qDiffs
+--       peAdd i _ pe = do
+--         peDelta <- pes `index` i
+--         write pes i (pe + peDelta)
+--       peFn i _ = do
+--         S.fill (peOnePairParticles i) (peAdd i) 0 I.nBodiesTwoPlanets
+--   S.fill (index qs) peFn 0 I.nBodiesTwoPlanets
+--   return pes
+
 hamiltonianP :: Double -> Masses -> Positions -> Momenta-> IO Double
 hamiltonianP gConst ms qs ps = do
   let preKes = dmap V.sum $ dzip2 (V.zipWith (*)) ps ps
       kes     = dzip2 (/) preKes (delay ms)
   ke <- walk (reduceL S.foldl (+)) (return 0) kes
-  return ke
+  pes :: Array Double <- new I.nBodiesTwoPlanets
+  let peOnePairParticles :: Int ->
+                            Int ->
+                            IO Double
+      peOnePairParticles i j
+        | i == j = return 0.0
+        | otherwise = do
+          q1 <- qs `index` i
+          m1 <- ms `index` i
+          q2 <- qs `index` j
+          m2 <- ms `index` j
+          let qDiffs = V.zipWith (-) q1 q2
+              dist2  = V.sum $ V.map (^2) qDiffs
+              a      = 1.0 / dist2
+              b      = (negate gConst) * m1 * m2 * (sqrt a)
+          return $ V.sum $ V.map (*b) qDiffs
+      peAdd i _ pe = do
+        peDelta <- pes `index` i
+        write pes i (pe + peDelta)
+      peFn i _ = do
+        S.fill (peOnePairParticles i) (peAdd i) 0 I.nBodiesTwoPlanets
+  S.fill (index qs) peFn 0 I.nBodiesTwoPlanets
+  pe <- walk (reduceL S.foldl (+)) (return 0) pes
+  return $ pe + ke
   
---   preKes <- sumP $ ps *^ ps
---   ke     <- sumP $ preKes /^ ms
---
---   ds2 <- sumP $ Repa.map (^2) $ pointDiffs qs
---   let ds   = Repa.map sqrt ds2
---       is   = prodPairsMasses ms
---       pess = zeroDiags $ Repa.map (* (negate gConst)) $ is /^ ds
---   pes <- sumP pess
---   pe  <- sumP pes
---   te :: Array U DIM0 Double <- computeP $ ke +^ pe
---   return $ head $ toList $ Repa.map (* 0.5) te
-
 main :: IO ()
 main = do
   ms <- masses
